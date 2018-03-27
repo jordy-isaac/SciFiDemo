4b188e1c424ae582951bd62b2924a26882a3e61a
Jordy Webb <jordan.webb@mapbox.com>
2018-03-27 05:13:32 -0700

    add inventory system

---
+++ Assets/Game/Scripts/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    [SerializeField]
+    private AudioClip _coinPickup;
+
+	void Update ()
+    {
+		
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+        if (other.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Player player = other.GetComponent<Player>();
+                if(player != null)
+                {
+                    player.hasCoin = true;
+                    AudioSource.PlayClipAtPoint(_coinPickup, transform.position, 1f);
+                    UIManager _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+                    if (_uiManager != null)
+                    {
+                        _uiManager.CollectedCoin();
+                    }
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+	}
+}
